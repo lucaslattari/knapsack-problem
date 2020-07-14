9888722f3572d6bbd6d980f2f38bfe9e7ac9d9d4
Lucas Lattari <lucas.lattari@ifsudestemg.edu.br>
2020-07-14 01:02:17 -0300

    added genetic algorithm

--- main.py
+++ main.py
@@ -68,6 +68,12 @@ def main():
         end = time.time()
         print("Tempo em segundos", end - start)
         print("Solução:", bestValue)
+    elif args.algorithm == "genetic":
+        start = time.time()
+        bestValue = computeKnapsackProblemGeneticAlgorithm(listOfValues, listOfWeights, capacity, args.totalItems)
+        end = time.time()
+        print("Tempo em segundos", end - start)
+        print("Solução:", bestValue)
 
 def computeKnapsackProblemDumbMethod(listOfValues, listOfWeights, capacity):
     import itertools as it
@@ -127,8 +133,147 @@ def computeKnapsackProblemDynamicProgramming(listOfValues, listOfWeights, maxCap
 
     return K[n][maxCapacity]
 
+import matplotlib.pyplot as plt
+def computeFitnessOfPopulation(listOfValues, listOfWeights, listOfPopulation, maxCapacity, numberOfItems, sizeOfPopulation):
+    fitness = []
+    for i in range(sizeOfPopulation):
+        valueOfPopulationI = np.sum(listOfPopulation[i] * listOfValues)
+        weightOfPopulationI = np.sum(listOfPopulation[i] * listOfWeights)
+
+        if weightOfPopulationI <= maxCapacity:
+            fitness = np.append(fitness, valueOfPopulationI)
+        else:
+            fitness = np.append(fitness, 0)
+
+    return fitness
+
+def getFittestIndividuals(fitnessForEachPopulation, numberOfParents, listOfPopulation):
+    fitnessForEachPopulationSorted = list(fitnessForEachPopulation)
+    fitnessForEachPopulationSorted.sort(reverse = True)
+    '''
+    print(fitnessForEachPopulation)
+    print("\n")
+    print(fitnessForEachPopulationSorted)
+    print("\n")
+    print(listOfPopulation)
+    print("\n")
+    '''
+
+    fittestIndividualsIndices = []
+    countParents = 0
+    while 1:
+        auxCandidateIndividuals = np.where(fitnessForEachPopulationSorted[countParents] == fitnessForEachPopulation)
+        for eachTuple in auxCandidateIndividuals:
+            for eachCandidate in eachTuple:
+                fittestIndividualsIndices = np.append(fittestIndividualsIndices, eachCandidate)
+                countParents += 1
+
+                if numberOfParents == countParents:
+                    #print(fittestIndividualsIndices)
+                    #print("\n")
+                    fittestIndividuals = np.zeros((numberOfParents, listOfPopulation.shape[1]), dtype = int) #linha, coluna
+                    row = 0
+                    for eachIndividualIndex in fittestIndividualsIndices: #TODO: enfiar row nesse for
+                        fittestIndividuals[row, :] = listOfPopulation[int(eachIndividualIndex)]
+                        row += 1
+                    return fittestIndividuals
+
+def generateChildByOnePointCrossover(fathers, numberOfChildren):
+    children = np.zeros((numberOfChildren, fathers.shape[1]), dtype = int) #linha, coluna
+
+    selectedFathers = random.sample(list(fathers), numberOfChildren)
+    for i in range(0, int(children.shape[0]), 2):
+        crossoverPoint = random.randint(1, fathers.shape[1] - 1)
+
+        randomFather1 = selectedFathers[i]
+        randomFather2 = selectedFathers[i + 1]
+
+        children[i, 0:crossoverPoint] = randomFather1[:crossoverPoint]
+        children[i, crossoverPoint:] = randomFather2[crossoverPoint:]
+
+        children[i + 1, 0:crossoverPoint] = randomFather2[:crossoverPoint]
+        children[i + 1, crossoverPoint:] = randomFather1[crossoverPoint:]
+        '''
+        print(randomFather1)
+        print(randomFather2)
+        print(crossoverPoint)
+        #print(randomFather1[:crossoverPoint])
+        #print(randomFather2[crossoverPoint:])
+        print(children[i])
+        print(children[i + 1])
+
+        input()
+        '''
+
+    return children
+
+def mutateIndividuals(individuals, mutationRate):
+    for individual in individuals:
+        randomValue = random.random()
+        if randomValue <= mutationRate:
+            sortedGene = random.randint(0, individual.shape[0] - 1)
+            if individual[sortedGene] == 0:
+                individual[sortedGene] = 1
+            else:
+                individual[sortedGene] = 0
+
 def computeKnapsackProblemGeneticAlgorithm(listOfValues, listOfWeights, maxCapacity, n):
     #https://medium.com/koderunners/genetic-algorithm-part-3-knapsack-problem-b59035ddd1d6
+
+    sizeOfPopulation = 32
+    numGenerations = 100
+    listOfPopulation = np.random.randint(2, size=(sizeOfPopulation, n))
+    fitnessHistory = []
+
+    for g in range(numGenerations):
+        fitnessForPopulation = computeFitnessOfPopulation(listOfValues, listOfWeights, listOfPopulation, maxCapacity, n, sizeOfPopulation)
+        fitnessHistory.append(fitnessForPopulation)
+
+        numberOfParents = int(sizeOfPopulation / 2)
+        fittestIndividuals = getFittestIndividuals(fitnessForPopulation, numberOfParents, listOfPopulation)
+        children = generateChildByOnePointCrossover(fittestIndividuals, numberOfParents)
+        mutationRate = 0.6
+        mutateIndividuals(children, mutationRate)
+
+        '''
+        print("Gen", g)
+        print(listOfPopulation)
+        print(fitnessForPopulation)
+        input()
+        '''
+
+        listOfPopulation[0 : fittestIndividuals.shape[0], :] = fittestIndividuals
+        listOfPopulation[fittestIndividuals.shape[0] :, :] = children
+
+        #print(listOfPopulation)
+        #print(computeFitnessOfPopulation(listOfValues, listOfWeights, listOfPopulation, maxCapacity, n, sizeOfPopulation))
+        #input()
+
+    finalFitness = computeFitnessOfPopulation(listOfValues, listOfWeights, listOfPopulation, maxCapacity, n, sizeOfPopulation)
+
+    print(listOfPopulation)
+    print(finalFitness)
+
+    fitnessHistoryMean = [np.mean(f) for f in fitnessHistory]
+    fitnessHistoryMax = [np.max(f) for f in fitnessHistory]
+
+    print(fitnessHistoryMean)
+    print(fitnessHistoryMax)
+
+    plt.plot(list(range(numGenerations)), fitnessHistoryMean, label = 'Mean Fitness')
+    plt.plot(list(range(numGenerations)), fitnessHistoryMax, label = 'Max Fitness')
+    plt.legend()
+    plt.title('Fitness through the generations')
+    plt.xlabel('Generations')
+    plt.ylabel('Fitness')
+    plt.show()
+
+    return max(finalFitness)
+
+    #fitness_history_max = [np.max(fitness) for fitness in fitness_history]
+
+    #print(listOfPopulation)
+    #print(fitnessForEachPopulation)
 
 #python main.py dumb 24 -w [382745,799601,909247,729069,467902,44328,34610,698150,823460,903959,853665,551830,610856,670702,488960,951111,323046,446298,931161,31385,496951,264724,224916,169684] -v [825594,1677009,1676628,1523970,943972,97426,69666,1296457,1679693,1902996,1844992,1049289,1252836,1319836,953277,2067538,675367,853655,1826027,65731,901489,577243,466257,369261] -W 6404180
 #python main.py recursive 24 -w [382745,799601,909247,729069,467902,44328,34610,698150,823460,903959,853665,551830,610856,670702,488960,951111,323046,446298,931161,31385,496951,264724,224916,169684] -v [825594,1677009,1676628,1523970,943972,97426,69666,1296457,1679693,1902996,1844992,1049289,1252836,1319836,953277,2067538,675367,853655,1826027,65731,901489,577243,466257,369261] -W 6404180
